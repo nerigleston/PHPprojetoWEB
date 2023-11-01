19e21dc840727286c160f2a062685035f5c97986
Neri Filho <nerifilho1256@gmail.com>
2023-11-01 12:24:53 -0300

    correção calulos

--- juros/script_juros_compostos.js
+++ juros/script_juros_compostos.js
@@ -1,26 +1,18 @@
 const btn = document.querySelector("#ver");
-btn.addEventListener("click",function(e) {
-e.preventDefault();
-})
+btn.addEventListener("click", function (e) {
+  e.preventDefault();
+});
 
-var pegartaxa="";
+var pegartaxa = "";
+var pegarcap = "";
+var pegarmeses = "";
 
-var pegarcap="";
+function vendo() {
+  pegarcap = document.getElementById('capital').value;
+  pegartaxa = document.getElementById('taxa').value;
+  pegarmeses = document.getElementById('meses').value;
 
-var pegarmeses="";
-
-function vendo(){
-
-pegarcap = document.getElementById('capital').value;
-
-pegartaxa = document.getElementById('taxa').value;
-
-pegarmeses = document.getElementById('meses').value;
-
-
-
-var montante= (pegarcap*(1+(pegartaxa/100))**pegarmeses);
-var montante= new Intl.NumberFormat('pt-BR', { style:'currency', currency: 'BRL'}).format(montante);
-document.getElementById('montantemo').innerHTML = montante;
-
+  var montante = pegarcap * (1 + (pegartaxa / 100)) ** pegarmeses;
+  montante = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(montante);
+  document.getElementById('montantemo').innerHTML = montante;
 }
--- juros/script_juros_simples.js
+++ juros/script_juros_simples.js
@@ -1,26 +1,19 @@
 const btn = document.querySelector("#ver");
 btn.addEventListener("click", function (e) {
    e.preventDefault();
-})
+});
 
 var pegartaxa = "";
-
 var pegarcap = "";
-
 var pegarmeses = "";
 
 function vendo() {
+   pegarcap = parseFloat(document.getElementById('capital').value);
+   pegartaxa = parseFloat(document.getElementById('taxa').value);
+   pegarmeses = parseInt(document.getElementById('meses').value);
 
-   pegarcap = document.getElementById('capital').value;
-
-   pegartaxa = document.getElementById('taxa').value;
-
-   pegarmeses = document.getElementById('meses').value;
-
-
-
-   var montante = (pegarcap * (1 + (pegartaxa / 100) * pegarmeses));
-   var montante = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(montante);
-   document.getElementById('montantemo').innerHTML = montante;
-
+   var juros = (pegarcap * (pegartaxa / 100) * pegarmeses);
+   var montante = pegarcap + juros;
+   var formattedMontante = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(montante);
+   document.getElementById('montantemo').innerHTML = formattedMontante;
 }
