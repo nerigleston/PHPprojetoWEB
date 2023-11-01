18f633a8d6c82c1e1c3168cd9f336d2a32de061f
Neri Filho <nerifilho1256@gmail.com>
2023-11-01 12:15:48 -0300

    modificado:protect.php

--- protect.php
+++ protect.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página Inválida</title>
     <link rel="stylesheet" type="text/css" href="./../styles/protect.css">
     <link rel="icon" href="./../assets/logo1.png" type="image/png">
 
